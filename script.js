ef536ab113862a8320f187e82d1df2f1ab7c0494
Farhan Zamir <fzamir1@myseneca.ca>
2026-05-11 14:32:34 -0400

    feat: add JavaScript with console output

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("CSP451 CheckPoint 1 site loaded.");
+
+function displayWelcomeMessage() {
+  console.log("Welcome to the GitHub and Version Control project.");
+}
+
+displayWelcomeMessage();
